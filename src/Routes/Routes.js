2495c940d05599612d503c23ddfd31655eebe5a6
antudas22 <20antu20@gmail.com>
2023-03-06 20:35:53 +0600

    project and router setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import Register from "../Pages/Register/Register";
+import SignIn from "../Pages/SignIn/SignIn";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>,
+        children:[
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/signin',
+                element: <SignIn/>
+            },
+            {
+                path: '/register',
+                element: <Register/>
+            },
+        ]
+    }
+])
